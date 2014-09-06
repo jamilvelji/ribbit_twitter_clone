9f03612e7bbff5c928fc56803032868c5439a50c
Jamil Velji <jamil@jamilvelji.com>
2014-09-06 17:52:42 -0400

    Now saving ribbits

--- app/controllers/ribbits_controller.rb
+++ app/controllers/ribbits_controller.rb
@@ -1,3 +1,15 @@
 class RibbitsController < ApplicationController
-  
+  def create
+    ribbit = Ribbit.new(ribbit_params)
+    ribbit.user_id = current_user.id
+
+      flash[:error] = "Your Ribbit was over 160 characters" unless ribbit.save
+      redirect_to root_url
+  end  
+
+  private
+
+  def ribbit_params
+    params.require(:ribbit).permit(:content)
+  end
 end
